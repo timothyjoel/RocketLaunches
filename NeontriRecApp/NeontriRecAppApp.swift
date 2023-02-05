a701fa0d0cd85994a35a37e8acd29c1b6a345d93
Tymoteusz Stokarski <tymoteusz.stokarski@proexe.pl>
2023-02-05 20:52:56 +0100

    Initial Commit

---
+++ NeontriRecApp/NeontriRecAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeontriRecAppApp.swift
+//  NeontriRecApp
+//
+//  Created by Timothy Stokarski on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NeontriRecAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
